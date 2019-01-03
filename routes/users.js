f6ee103dc83f76c96292bf50cd7e6d799cdfa6a8
bartek <Bartek.NNAAN@wp.pl>
2019-01-03 20:48:32 +0100

    autentication bcrypt hashing password, adding users to mongoDB

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+//User model
+const User = require('../models/User');
 
 //Login Page
 router.get('/login', (req, res, next) => {
@@ -36,7 +39,49 @@ router.post('/register', (req, res, next) => {
             password2
         });
     } else {
-        res.send('pass');
+        //Validation passed
+        User.findOne({ email: email })
+            .then(user => {
+                if (user) {
+                    // User exists
+                    errors.push({ msg: 'Email is already registered' });
+                    res.render('register', {
+                        errors,
+                        name,
+                        email,
+                        password,
+                        password2
+                    }); 
+                } else {
+                    const newUser = new User({
+                        name,
+                        email,
+                        password
+                    });
+                    //Hash Password
+                    bcrypt.genSalt(12, (err, salt) => {
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            if(err) {
+                                throw err;
+                            }
+                            //Set password to hashed
+                            newUser.password = hash;
+                            //Save user
+                            newUser
+                            .save()
+                            .then(user => {
+                                res.redirect('/user/login');
+                            })
+                            .catch(err => {
+                                throw err;
+                            })
+                        })
+                    })
+                }
+            })
+            .catch(err => {
+                throw err;
+            });
     }
 
 });
